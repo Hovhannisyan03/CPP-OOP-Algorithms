5865c7006f3a6fa2b3948490b1237a15849156bf
Sargis <138877136+Hovhannisyan03@users.noreply.github.com>
2024-07-26 10:47:08 +0400

    Update queue.hpp

--- Algorithms/data_structures/queue/queue/queue.hpp
+++ Algorithms/data_structures/queue/queue/queue.hpp
@@ -1,131 +1,179 @@
 #include "queue.h"
 
-template <typename T>
-queue<T>::queue() : m_arr() {}
+template <typename T, int N>
+my_std::queue<T,N>::queue() : m_arr(N), m_front(0), m_reArr(0), m_size(-1){}
 
-template <typename T>
-queue<T>::queue(const queue& other) : m_arr(other.m_arr){}
+template <typename T, int N>
+my_std::queue<T,N>::queue(const queue& other) : m_arr(other.m_arr), m_front(other.m_front), m_reArr(other.m_reArr), m_size(other.m_size){}
 
-template <typename T>
-queue<T>::queue(queue&& other) noexcept : m_arr(std::move(other.m_arr)) {}
-
-template <typename T>
-queue<T>::queue(std::initializer_list<value_type> init) : m_arr(init){}
-
-template <typename T>
-queue<T>::~queue()
+template <typename T, int N>
+my_std::queue<T,N>::queue(queue&& other) noexcept : m_arr(std::move(other.m_arr)), m_front(std::move(other.m_front)), m_reArr(std::move(other.m_reArr)), m_size(std::move(other.m_size))
 {
-    m_arr.clear();
+    other.m_size = -1;
 }
 
-template <typename T>
-const queue<T>& queue<T>::operator=(const queue& other) 
+template <typename T, int N>
+my_std::queue<T,N>::queue(std::initializer_list<value_type> range) : m_arr(N), m_front(0), m_reArr(0), m_size(-1)
+{
+    for(auto elem : range)
+    {
+        if(m_size <= N-1)
+        {
+            m_arr[m_reArr] = elem;
+            m_reArr = (m_reArr + 1) % N;
+            ++m_size;
+        }
+        else
+        {
+            std::cout << "Out of range" << std::endl;
+            throw std::out_of_range("Out of range");
+        }
+    }
+}
+
+template <typename T, int N>
+my_std::queue<T,N>::~queue(){}
+
+template <typename T, int N>
+const typename my_std::queue<T,N>::queue& my_std::queue<T,N>::operator=(const queue& other)
 {
     if(this != &other)
     {
         m_arr = other.m_arr;
+        m_front = other.m_front;
+        m_reArr = other.m_reArr;
+        m_size = other.m_size;
     }
 
     return *this;
 }
 
-template <typename T>
-const queue<T>& queue<T>::operator=(queue&& other) noexcept
+template <typename T, int N>
+const typename my_std::queue<T,N>::queue& my_std::queue<T,N>::operator=(queue&& other) noexcept
 {
     if(this != &other)
     {
         m_arr = std::move(other.m_arr);
+        m_front = std::move(other.m_front);
+        m_reArr = std::move(other.m_reArr);
+        m_size = std::move(other.m_size);
+        other.m_size = -1;
     }
 
     return *this;
 }
 
-
-template <typename T>
-typename queue<T>::referance queue<T>::front()
+template <typename T, int N>
+bool my_std::queue<T,N>::empty() const
 {
-    return m_arr.front();
+    if(m_size == -1)
+    {
+        return true;
+    }
+    return false;
 }
 
-template <typename T>
-typename queue<T>::const_referance queue<T>::front() const
+template <typename T, int N>
+typename my_std::queue<T,N>::size_type my_std::queue<T,N>::size() const
 {
-    return m_arr.front();
+    return m_size + 1;
+}
+
+template <typename T, int N>
+void my_std::queue<T,N>::enqueue(const_referance val)
+{
+    if(m_size >= N-1)
+    {
+        std::cout << "Queue is full" << std::endl;
+        throw std::out_of_range("Queue is full");
+    }
+    m_arr[m_reArr] = val;
+    m_reArr = (m_reArr + 1) % N;
+    ++m_size;
+}
+
+template <typename T, int N>
+void my_std::queue<T,N>::dequeue()
+{
+    if(empty())
+    {
+        std::cout << "Queue is empty" << std::endl;
+        throw std::logic_error("Queue is empty");
+    }
+    m_front = (m_front + 1) % N;
+    --m_size;
+}
+
+template <typename T, int N>
+typename my_std::queue<T,N>::referance my_std::queue<T,N>::front()
+{
+    if(empty())
+    {
+        std::cout << "Queue is empty" << std::endl;
+        throw std::logic_error("Queue is empty");
+    }
+    return m_arr[m_front];
+}
+
+template<typename T, int N>
+typename my_std::queue<T,N>::const_referance my_std::queue<T,N>::front() const
+{
+    if(empty())
+    {
+        std::cout << "Queue is empty" << std::endl;
+        throw std::logic_error("Queue is empty");
+    }
+    return m_arr[m_front];
 }
 
 
-template <typename T>
-typename queue<T>::referance queue<T>::back()
+template<typename T, int N>
+void my_std::queue<T,N>::print() const
 {
-    return m_arr.back();
+    if(empty())
+    {
+        std::cout << "Queue is empty" << std::endl;
+        return;
+    }
+    for(int i = 0; i <= m_size; ++i)
+    {
+        std::cout << m_arr[(m_front + i) % N] << " ";
+    }
+    std::cout << std::endl;
 }
 
-template <typename T>
-typename queue<T>::const_referance queue<T>::back() const
-{
-    return m_arr.back();
-}
-
-template <typename T>
-bool queue<T>::empty() const
-{
-    return m_arr.empty();
-}
-
-template <typename T>
-void queue<T>::enqueue(const_referance val)
-{
-    m_arr.push_back(val);
-}
-
-template <typename T>
-void queue<T>::dequeue()
-{
-    m_arr.pop_front();
-}
-
-template <typename T>
-typename queue<T>::size_type queue<T>::size() const
-{
-    return m_arr.size();
-}
-
-
-template <typename T>
-bool queue<T>::operator==(const queue& other)
+template <typename T, int N>
+bool my_std::queue<T,N>::operator==(const queue& other)
 {
     return m_arr == other.m_arr;
 }
 
-template <typename T>
-bool queue<T>::operator!=(const queue& other)
+template <typename T, int N>
+bool my_std::queue<T,N>::operator!=(const queue& other)
 {
     return m_arr != other.m_arr;
 }
 
-template <typename T>
-bool queue<T>::operator<(const queue& other)
+template <typename T, int N>
+bool my_std::queue<T,N>::operator<(const queue& other)
 {
     return m_arr < other.m_arr;
 }
 
-template <typename T>
-bool queue<T>::operator<=(const queue& other)
+template <typename T, int N>
+bool my_std::queue<T,N>::operator<=(const queue& other)
 {
     return m_arr <= other.m_arr;
 }
 
-template <typename T>
-bool queue<T>::operator>(const queue& other)
+template <typename T, int N>
+bool my_std::queue<T,N>::operator>(const queue& other)
 {
     return m_arr > other.m_arr;
 }
 
-template <typename T>
-bool queue<T>::operator>=(const queue& other)
+template <typename T, int N>
+bool my_std::queue<T,N>::operator>=(const queue& other)
 {
     return m_arr >= other.m_arr;
 }
-
-
-
