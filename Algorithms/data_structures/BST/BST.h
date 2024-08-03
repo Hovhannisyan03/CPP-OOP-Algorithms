42f96d8cb24bcad4df556cc30b004fdb532e1fc3
Sargis <138877136+Hovhannisyan03@users.noreply.github.com>
2024-08-03 23:51:42 +0400

    Update BST.h

--- Algorithms/data_structures/BST/BST.h
+++ Algorithms/data_structures/BST/BST.h
@@ -5,6 +5,13 @@
 #include <iostream>
 #include <stack>
 #include <queue>
+
+template <typename T>
+void print(T& val)
+{
+    std::cout << val << " ";
+}
+
 namespace my_std
 {
     template <typename T>
@@ -13,7 +20,9 @@ namespace my_std
         public:
             using value_type = T;
             using size_type = size_t;
-
+            using reference = value_type &;
+            using const_reference = const value_type &;
+            using predicate = void(*)(T&);
         private:
             struct Node
             {
@@ -26,6 +35,7 @@ namespace my_std
        
         public:
             binary_search_tree();
+            binary_search_tree(const_reference val);
             binary_search_tree(const binary_search_tree& rhv);
             binary_search_tree(binary_search_tree&& rhv) noexcept;
             binary_search_tree(std::initializer_list<value_type> init);
@@ -41,13 +51,14 @@ namespace my_std
             size_type get_height() const;
             void insert(value_type val);
             void remove(value_type val);
-            void inorder(bool recursive = true) const;
-            void preorder(bool recursive = true) const;
-            void postorder(bool recursive = true) const;
+            void inorder(predicate visit = print, bool iterative = false) const;
+            void preorder(predicate visit = print, bool iterative = false) const;
+            void postorder(predicate visit = print, bool iterative = false) const;
             size_type get_number_of_nodes() const;
             value_type get_root_data() const;
             value_type get_Entry(value_type val) const;
             bool contains(value_type val) const;
+            void level_order(predicate visit = print) const;
 
         private:
             Node* m_root;
@@ -71,16 +82,16 @@ namespace my_std
             Node* m_remove_recursive(Node* node, value_type val); 
             Node* m_remove_iterative(Node* node, value_type val); 
 
-            void m_inorder_recursive(Node* node) const;
-            void m_inorder_iterative(Node* node) const;
+            void m_inorder_recursive(Node* node, predicate visit) const;
+            void m_inorder_iterative(Node* node, predicate visit) const;
 
-            void m_preorder_recursive(Node* node) const;
-            void m_preorder_iterative(Node* node) const;
+            void m_preorder_recursive(Node* node, predicate visit) const;
+            void m_preorder_iterative(Node* node, predicate visit) const;
             
-            void m_postorder_recursive(Node* node) const;    
-            void m_postorder_iterative(Node* node) const;
+            void m_postorder_recursive(Node* node, predicate visit) const;    
+            void m_postorder_iterative(Node* node, predicate visit) const;
 
-            void m_level_order_iterative(Node* node) const;
+            void m_level_order_iterative(Node* node, predicate visit) const;
 
             Node* m_get_Entry_recursive(Node* node, value_type val) const;
             Node* m_get_Entry_iterative(Node* node, value_type val) const;  
