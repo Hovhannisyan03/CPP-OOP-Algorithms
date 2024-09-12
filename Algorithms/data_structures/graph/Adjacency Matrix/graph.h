c429b4de3c66ab3b2c9ffcaa8774d39685051653
Sargis <138877136+Hovhannisyan03@users.noreply.github.com>
2024-09-12 23:41:18 +0400

    Update graph.h

--- Algorithms/data_structures/graph/Adjacency Matrix/graph.h
+++ Algorithms/data_structures/graph/Adjacency Matrix/graph.h
@@ -22,20 +22,21 @@ namespace my_graph
             void DFS_R() const;
             void DFS_I() const;
             void BFS() const;
-            size_type getCountNthLevel(value_type v, value_type level) const;
-            // value_type getCountNthLevelWithDFS(value_type v, value_type level) const;
+            size_type getCountNthLevel(value_type v, size_type level) const;
+            size_type getCountNthLevelWithDFS(value_type v, size_type level) const;
             // void transpose();//Uncomment for directed graph
             
-            // std::vector<value_type> get_shortest_path(value_type u, value_type v);
-            // std::vector<std::vector<value_type>> get_all_possible_paths(value_type src, value_type dest) const;
+            std::vector<value_type> get_shortest_path(value_type u, value_type v) const;
+            std::vector<std::vector<value_type>> get_all_possible_paths(value_type src, value_type dest) const;
 
         private:
             std::vector<std::vector<bool>> m_adjacency_matrix;
             void m_DFS_Rec_helper(value_type v, std::vector<bool>& visited) const;
             void m_DFS_Iter_helper(value_type v, std::vector<bool>& visited) const;
 
-            // void m_getCountNthLevelWithDFS_helper(value_type v, value_type currLevel, value_type level, value_type& count, std::vector<bool>& visited) const;
-            // void dfsAllPaths(value_type src, value_type dest, std::vector<value_type>& tmp, std::vector<std::vector<value_type> >& res, std::vector<bool>& visit) const;
+            std::vector<value_type> m_get_shortest_path_helper(std::vector<value_type>& parent, value_type v) const;
+            void m_getCountNthLevelWithDFS_helper(value_type v, size_type current_level, size_type level, size_type& count, std::vector<bool>& visited) const;
+            void m_get_all_possible_paths_helper(value_type u, value_type v, std::vector<value_type>& path, std::vector<std::vector<value_type>>& all_paths, std::vector<bool>& visited) const;
     };
 }
 #include "graph.hpp"
