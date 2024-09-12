4a6c5171617c5f477ba09e4d90748c8d479c3774
Sargis <138877136+Hovhannisyan03@users.noreply.github.com>
2024-09-12 23:41:43 +0400

    Update graph.hpp

--- Algorithms/data_structures/graph/Adjacency Matrix/graph.hpp
+++ Algorithms/data_structures/graph/Adjacency Matrix/graph.hpp
@@ -108,7 +108,7 @@ namespace my_graph
 
         for(size_type i = 0; i < m_adjacency_matrix.size(); ++i)
         {
-            if(m_adjacency_matrix[v][i] == true && !visited[i])
+            if(m_adjacency_matrix[v][i] && !visited[i])
             {
                 m_DFS_Rec_helper(i,visited);
             }
@@ -119,9 +119,9 @@ namespace my_graph
     void Graph<T>::BFS() const 
     {
         std::vector<bool> visited(m_adjacency_matrix.size(), false);
-        std::queue<int> q;
+        std::queue<value_type> q;
         
-        for(int i = 0; i < m_adjacency_matrix.size(); ++i) 
+        for(size_type i = 0; i < m_adjacency_matrix.size(); ++i) 
         {
             if(!visited[i]) 
             {
@@ -130,13 +130,13 @@ namespace my_graph
 
                 while(!q.empty()) 
                 {
-                    int v = q.front();
+                    value_type v = q.front();
                     q.pop();
                     std::cout << v << " ";
 
-                    for (int j = 0; j < m_adjacency_matrix.size(); ++j) 
+                    for (size_type j = 0; j < m_adjacency_matrix.size(); ++j) 
                     {
-                        if (m_adjacency_matrix[v][j] == 1 && !visited[j]) 
+                        if (m_adjacency_matrix[v][j] && !visited[j]) 
                         {
                             visited[j] = true;
                             q.push(j);
@@ -150,8 +150,13 @@ namespace my_graph
 
 
     template <typename T>
-    typename Graph<T>::size_type Graph<T>::getCountNthLevel(value_type v, value_type level) const
+    typename Graph<T>::size_type Graph<T>::getCountNthLevel(value_type v, size_type level) const
     {
+        if (v >= m_adjacency_matrix.size())
+        {
+            throw std::out_of_range("Vertex out of range");
+        }
+
         std::vector<bool> visited(m_adjacency_matrix.size(), false);
         std::queue<value_type> q;
         q.push(v);
@@ -167,7 +172,6 @@ namespace my_graph
             {
                 return levelSize;
             }
-
             for (size_type i = 0; i < levelSize; ++i) 
             {
                 value_type u = q.front();
@@ -182,11 +186,134 @@ namespace my_graph
                     }
                 }
             }
-
             ++currentLevel;
         }
-
         return 0;
+    }
+
+     template <typename T>
+    typename Graph<T>::size_type Graph<T>::getCountNthLevelWithDFS(value_type v, size_type level) const
+    {
+        if(v >= m_adjacency_matrix.size())
+        {
+            throw std::out_of_range("Out of range");
+        }
+
+        std::vector<bool> visited(m_adjacency_matrix.size(), false);
+        size_type count = 0;
+
+        m_getCountNthLevelWithDFS_helper(v, 0, level, count, visited);
+        return count;
+    }
+
+    template <typename T>
+    void Graph<T>::m_getCountNthLevelWithDFS_helper(value_type v, size_type current_level, size_type level, size_type& count, std::vector<bool>& visited) const
+    {
+        visited[v] = true;
+
+        if(current_level == level)
+        {
+            ++count;
+        }
+        if(current_level > level)
+        {
+            return;
+        }
+
+        for(size_type i = 0; i < m_adjacency_matrix.size(); ++i)
+        {
+            if(m_adjacency_matrix[v][i] && !visited[i])
+            {
+                m_getCountNthLevelWithDFS_helper(i, current_level + 1, level, count, visited);
+            }
+        }
+    }
+
+    template <typename T>
+    std::vector<typename Graph<T>::value_type> Graph<T>::get_shortest_path(value_type u, value_type v) const
+    {
+        std::vector<bool> visited(m_adjacency_matrix.size(),false);
+        std::queue<value_type> q;
+        std::vector<value_type> parent(m_adjacency_matrix.size(), - 1);
+        q.push(u);
+        visited[u] = true;
+        while(!q.empty())
+        {
+            value_type vertex = q.front();
+            q.pop();
+
+            if (vertex == v) 
+            {
+                break;
+            }
+
+            for(size_type i = 0; i < m_adjacency_matrix.size(); ++i)
+            {
+                if(m_adjacency_matrix[vertex][i] && !visited[i])
+                {
+                    visited[i] = true;
+                    parent[i] = vertex;
+                    q.push(i);
+                }
+            }
+        }
+    
+        return m_get_shortest_path_helper(parent,v);
+    }
+
+    template <typename T>
+    std::vector<typename Graph<T>::value_type> Graph<T>::m_get_shortest_path_helper(std::vector<value_type>& parent, value_type v) const
+    {    
+        if(parent[v] == -1)
+        {
+            return {};
+        }
+        std::vector<value_type> path;
+        while(parent[v] != -1)
+        {       
+            path.push_back(v);
+            v = parent[v];
+        }
+        path.push_back(v);
+
+        std::reverse(path.begin(), path.end());
+        return path;
+    }
+
+    template <typename T>
+    std::vector<std::vector<typename Graph<T>::value_type>> Graph<T>::get_all_possible_paths(value_type u, value_type v) const
+    {
+        std::vector<bool> visited(m_adjacency_matrix.size(), false);
+        std::vector<value_type> path;
+        std::vector<std::vector<value_type>> all_paths;
+        
+        m_get_all_possible_paths_helper(u, v, path, all_paths, visited);
+        return all_paths;
+
+    }
+
+    template <typename T>
+    void Graph<T>::m_get_all_possible_paths_helper(value_type u, value_type v, std::vector<value_type>& path, std::vector<std::vector<value_type>>& all_paths, std::vector<bool>& visited) const
+    {
+        visited[u] = true;
+        path.push_back(u);
+        if(u == v)
+        {
+            all_paths.push_back(path);
+            path.pop_back();
+            visited[u] = false;
+            return;
+        }
+        for(size_type i = 0; i < m_adjacency_matrix.size(); ++i)
+        {
+            if(m_adjacency_matrix[u][i] && !visited[i])
+            {  
+                visited[i] = true;
+                m_get_all_possible_paths_helper(i,v,path,all_paths,visited);
+            }
+        }
+        visited[u] = false;
+        path.pop_back();
     }
 
     //Uncomment for directed graph
